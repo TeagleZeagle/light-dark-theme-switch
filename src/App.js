e2131d61a9118249bc23885b3efd1f93b00de3ce
Kaan Kani İnce <kkince@newmind.ai>
2024-07-08 11:41:06 +0300

    light-dark-mode-created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import LightDarkMode from "./components/light-dark-mode";
+
+function App() {
+  return (
+    <div className="App">
+      {/* light and dark theme switch*/}
+      <LightDarkMode/>
+    </div>
+  );
+}
+
+export default App;
